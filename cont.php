c0a8f7abb3dd3f019e685dc29c58006e68a2a1ce
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2021-06-03 22:43:23 +0800

    Initial commit

---
+++ cont.php
@@ -0,0 +1,33 @@
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $res;
+ 
+  $ftp_username = "anonymous";
+  $ftp_userpass = "guest";
+  $url = "ftp.bom.gov.au";
+  $local_file = "IDW12300.xml";
+  $server_file = "/anon/gen/fwo/IDW12300.xml";
+  $local_file0 = "IDW60920.xml";
+  $server_file0 = "/anon/gen/fwo/IDW60920.xml";
+  $fp = fopen($local_file,"w");
+  $fp0 = fopen($local_file0,"w");
+
+  $ftp = ftp_connect($url);
+  ftp_login($ftp, $ftp_username, $ftp_userpass );
+  if (ftp_fget($ftp, $fp, $server_file, FTP_BINARY, 0)) {
+    if (ftp_fget($ftp, $fp0, $server_file0, FTP_BINARY, 0)) {
+      $res = 1;
+    } else {
+      $res = print_r(error_get_last()['message']);
+    }
+  } else {
+    $res = print_r(error_get_last()['message']);
+  }
+  ftp_close($ftp);
+  fclose($fp);
+
+  echo $res;
+}
+?>
+
---
+++ js/scripts.js
@@ -0,0 +1,95 @@
+
+function events (obj,typ,opts) {
+    if (obj) {
+        obj.addEventListener(typ,callback,opts);
+    }
+}
+
+window.onload = function () {
+
+    let parser = new DOMParser();
+    let xmlDoc;
+    let xmlDoc0;
+    var xmlhttp = new XMLHttpRequest();
+        xmlhttp.open("POST", "cont.php", true);
+        xmlhttp.onload = function() {
+        if (xmlhttp.status == 200) {
+            if (this.responseText == 1) {
+                let xmlFile0 = 'IDW60920.xml';
+                let xmlFile = 'IDW12300.xml';
+                xmlhttp.open("POST", xmlFile0, true);
+                xmlhttp.onload = function() {
+                    if (xmlhttp.status == 200) {
+                        xmlDoc0 = parser.parseFromString(this.response,"text/xml");
+                        xmlhttp.open("POST", xmlFile, true);
+                        xmlhttp.onload = function() {
+                            if (xmlhttp.status == 200) {
+                                xmlDoc = parser.parseFromString(this.response,"text/xml");
+                                for (let x = 0; x <= 6; x++){
+                                    let txt = "<br>";
+                                    let y = xmlDoc.getElementsByTagName("product")[0].getElementsByTagName("forecast")[0].getElementsByTagName("area")[2].getElementsByTagName("forecast-period")[x].getElementsByTagName("element");
+                                    for (i = 0; i < y.length; i++) {
+                                        if (y[i].getAttribute('type') === "forecast_icon_code") { 
+                                            document.getElementsByClassName("x")[x].src = "./images/" + y[i].childNodes[0].nodeValue  + ".png"
+                                        }
+                                        if (y[i].hasAttribute("units")) {
+                                            if (y[i].getAttribute('type') === "air_temperature_minimum") {
+                                                txt += "Minimum&nbsp;" + y[i].childNodes[0].nodeValue + "&nbsp;°C<br>";
+                                            }
+                                            if (y[i].getAttribute('type') === "air_temperature_maximum") {
+                                                txt += "Maximum&nbsp;" + y[i].childNodes[0].nodeValue + "&nbsp;°C<br>";
+                                            }
+                                        }
+                                    }
+                                    let txt0 = "<br>"
+                                    let u = xmlDoc0.getElementsByTagName("product")[0].getElementsByTagName("observations")[0].getElementsByTagName("station")[0].getElementsByTagName("period")[0].getElementsByTagName("level")[0].getElementsByTagName("element");
+                                    for (i = 0; i < u.length; i++) {
+                                        if (u[i].hasAttribute("units")) {
+                                            if (u[i].getAttribute('type') === "minimum_air_temperature") {
+                                                txt0 += "Current Minimum&nbsp;" + u[i].childNodes[0].nodeValue + "&nbsp;°C<br>";
+                                            }
+                                            if (u[i].getAttribute('type') === "maximum_air_temperature") {
+                                                txt0 += "Currrent Maximum&nbsp;" + u[i].childNodes[0].nodeValue + "&nbsp;°C<br>";
+                                            }
+                                            if (u[i].getAttribute('type') === "air_temperature") {
+                                                txt0 += "Current&nbsp;" + u[i].childNodes[0].nodeValue + "&nbsp;°C<br>";
+                                            }
+                                        }
+                                    }
+                                    let date = new Date(xmlDoc.getElementsByTagName("product")[0].getElementsByTagName("forecast")[0].getElementsByTagName("area")[2].getElementsByTagName("forecast-period")[x].getAttribute('start-time-local'))
+                                    date.toLocaleDateString()
+                                    if (x < 1) {
+                                        document.getElementsByClassName("f")[x].innerHTML = date.toLocaleDateString()
+                                        document.getElementsByClassName("z")[x].innerHTML = xmlDoc.getElementsByTagName("product")[0].getElementsByTagName("forecast")[0].getElementsByTagName("area")[1].getElementsByTagName("forecast-period")[x].getElementsByTagName("text")[0].childNodes[0].nodeValue + txt0;
+                                        document.getElementsByClassName("y")[x].innerHTML = xmlDoc.getElementsByTagName("product")[0].getElementsByTagName("forecast")[0].getElementsByTagName("area")[1].getElementsByTagName("forecast-period")[x].getElementsByTagName("text")[0].childNodes[0].nodeValue + txt0;
+                                       
+                                    } else if (x >= 1) {
+                                          document.getElementsByClassName("f")[x].innerHTML = date.toLocaleDateString()
+                                          document.getElementsByClassName("z")[x].innerHTML = xmlDoc.getElementsByTagName("product")[0].getElementsByTagName("forecast")[0].getElementsByTagName("area")[1].getElementsByTagName("forecast-period")[x].getElementsByTagName("text")[0].childNodes[0].nodeValue + txt;
+                                          document.getElementsByClassName("y")[x].innerHTML = xmlDoc.getElementsByTagName("product")[0].getElementsByTagName("forecast")[0].getElementsByTagName("area")[1].getElementsByTagName("forecast-period")[x].getElementsByTagName("text")[0].childNodes[0].nodeValue + txt;
+                                    }
+                                 }
+                            
+                            } else {
+                                console.log(this.status)
+                            }                   
+                         }
+                         xmlhttp.send();
+                    } else {
+                        console.log(this.status)
+                    }                   
+                }
+                xmlhttp.send();
+            } else {
+                console.log(this.responseText)
+            }
+        }
+        else {
+            console.log(this.status)
+        }
+    }
+    xmlhttp.send();
+}
+    
+    
+
