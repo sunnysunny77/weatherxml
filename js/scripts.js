8e8722d0b85beba53d8064e1e511fa7a2c1600b9
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2021-06-04 00:43:31 +0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -60,7 +60,7 @@ window.onload = function () {
                                     } else if (x >= 1) {
                                         document.getElementsByClassName("f")[x].innerHTML = date.toLocaleDateString()
                                         document.getElementsByClassName("z")[x].innerHTML = p.childNodes[0].nodeValue + txt;
-                                        document.getElementsByClassName("y")[x].innerHTML = p.nodeValue + txt;
+                                        document.getElementsByClassName("y")[x].innerHTML = p.childNodes[0].nodeValue + txt;
                                     }
                                 }
                             } else {
