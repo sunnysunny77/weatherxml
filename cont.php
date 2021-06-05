e268a088b2b2324ecfdaccca68d274740e9e5d81
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2021-06-05 14:39:18 +0800

    up

--- cont.php
+++ cont.php
@@ -10,23 +10,35 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $server_file = "/anon/gen/fwo/IDW12300.xml";
   $local_file0 = "IDW60920.xml";
   $server_file0 = "/anon/gen/fwo/IDW60920.xml";
+
   $fp = fopen($local_file,"w");
   $fp0 = fopen($local_file0,"w");
 
   $ftp = ftp_connect($url);
   ftp_login($ftp, $ftp_username, $ftp_userpass );
   ftp_pasv($ftp, true) or die("Cannot switch to passive mode");
-  if (ftp_fget($ftp, $fp, $server_file, FTP_BINARY, 0)) {
+
+  if (!file_exists($server_file) || time()-filemtime($server_file) > 0.5 * 3600) {
+    if (ftp_fget($ftp, $fp, $server_file, FTP_BINARY, 0)) {
+      if (ftp_fget($ftp, $fp0, $server_file0, FTP_BINARY, 0)) {
+        $res = var_dump(http_response_code(200));
+      } else {
+        $res = print_r(error_get_last()['message']);
+        http_response_code(500);
+      }
+    } else {
+      $res = print_r(error_get_last()['message']);
+      http_response_code(500);
+    }
+  } else {
     if (ftp_fget($ftp, $fp0, $server_file0, FTP_BINARY, 0)) {
       $res = var_dump(http_response_code(200));
     } else {
       $res = print_r(error_get_last()['message']);
       http_response_code(500);
     }
-  } else {
-    $res = print_r(error_get_last()['message']);
-    http_response_code(500);
   }
+  
   ftp_close($ftp);
   fclose($fp);
 
