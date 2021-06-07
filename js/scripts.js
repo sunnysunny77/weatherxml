7be4ac2fc0b50e9f81701e2e368dd681938771a1
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2021-06-07 13:13:41 +0800

    up

--- js/scripts.js
+++ js/scripts.js
@@ -18,48 +18,61 @@ window.onload = function () {
                         if (xmlhttp.status == 200) {
                             xmlDoc = parser.parseFromString(this.response,"text/xml");
                             let u = xmlDoc0.getElementsByTagName("product")[0].getElementsByTagName("observations")[0].getElementsByTagName("station")[0].getElementsByTagName("period")[0].getElementsByTagName("level")[0].getElementsByTagName("element");
+                            let t = xmlDoc.getElementsByTagName("product")[0].getElementsByTagName("forecast")[0].getElementsByTagName("area");
                             for (let x = 0; x <= 6; x++){
-                                let t = xmlDoc.getElementsByTagName("product")[0].getElementsByTagName("forecast")[0].getElementsByTagName("area")[2].getElementsByTagName("forecast-period")[x];
-                                let y = t.getElementsByTagName("element");
-                                let o = t.getElementsByTagName("text");
-                                let date = new Date(t.getAttribute('start-time-local'));
-                                date = date.toLocaleDateString();
-                                let p = xmlDoc.getElementsByTagName("product")[0].getElementsByTagName("forecast")[0].getElementsByTagName("area")[1].getElementsByTagName("forecast-period")[x].getElementsByTagName("text")[0];
-                                let txt0 = "<br>";
                                 let txt = "<br>";
-                                for (i = 0; i < y.length; i++) {
-                                    if (y[i].getAttribute('type') === "forecast_icon_code") { 
-                                        document.getElementsByClassName("x")[x].src = "./images/" + y[i].childNodes[0].nodeValue  + ".png";
+                                let txt0 = "<br>";
+                                let txt1 = "<br>";
+                                let date;
+                                for (i = 0; i < t.length; i++) {
+                                    if (t[i].getAttribute('aac') === "WA_ME001") {
+                                        let q = t[i].getElementsByTagName("forecast-period")[x].getElementsByTagName("text");
+                                        for (l = 0; l < q.length; l++) {
+                                            if (q[l].getAttribute('type') === "forecast") {
+                                                txt += q[l].childNodes[0].nodeValue
+                                            }
+                                        }
+                                        date = new Date(t[i].getElementsByTagName("forecast-period")[x].getAttribute('start-time-local'))
+                                        date = date.toLocaleDateString();
                                     }
-                                    if (y[i].getAttribute('type') === "air_temperature_minimum") {
-                                        txt += "Minimum&nbsp;" + y[i].childNodes[0].nodeValue + "&nbsp;°C<br>";
-                                    }
-                                    if (y[i].getAttribute('type') === "air_temperature_maximum") {
-                                        txt += "Maximum&nbsp;" + y[i].childNodes[0].nodeValue + "&nbsp;°C<br>";
-                                    }
-                                }
-                                for (m = 0; m < o.length; m++) {
-                                    if (o[m].getAttribute('type') === "precis") {
-                                        document.getElementsByClassName("x")[x].alt = o[m].childNodes[0].nodeValue; 
+                                    if (t[i].getAttribute('aac') === "WA_PT053") {
+                                        let q = t[i].getElementsByTagName("forecast-period")[x].getElementsByTagName("element");
+                                        for (l = 0; l < q.length; l++) {
+                                            if (q[l].getAttribute('type') === "forecast_icon_code") { 
+                                                document.getElementsByClassName("x")[x].src = "./images/" + q[l].childNodes[0].nodeValue  + ".png";
+                                            }
+                                            if (q[l].getAttribute('type') === "air_temperature_minimum") {
+                                                txt0 += "Minimum&nbsp;" + q[l].childNodes[0].nodeValue + "&nbsp;°C<br>";
+                                            }
+                                            if (q[l].getAttribute('type') === "air_temperature_maximum") {
+                                                txt0 += "Maximum&nbsp;" + q[l].childNodes[0].nodeValue + "&nbsp;°C";
+                                            }
+                                        }
+                                        let c =  t[i].getElementsByTagName("forecast-period")[x].getElementsByTagName("text");
+                                        for (l = 0; l < c.length; l++) {
+                                            if (c[l].getAttribute('type') === "precis") {
+                                                document.getElementsByClassName("x")[x].alt = c[l].childNodes[0].nodeValue; 
+                                            }
+                                        }
                                     }
                                 }
                                 for (i = 0; i < u.length; i++) {
                                     if (u[i].getAttribute('type') === "minimum_air_temperature") {
-                                        txt0 += "Current Minimum&nbsp;" + u[i].childNodes[0].nodeValue + "&nbsp;°C<br>";
+                                        txt1 += "Current Minimum&nbsp;" + u[i].childNodes[0].nodeValue + "&nbsp;°C<br>";
                                     }
                                     if (u[i].getAttribute('type') === "maximum_air_temperature") {
-                                        txt0 += "Currrent Maximum&nbsp;" + u[i].childNodes[0].nodeValue + "&nbsp;°C<br>";
+                                        txt1 += "Currrent Maximum&nbsp;" + u[i].childNodes[0].nodeValue + "&nbsp;°C<br>";
                                     }
                                     if (u[i].getAttribute('type') === "air_temperature") {
-                                        txt0 += "Current&nbsp;" + u[i].childNodes[0].nodeValue + "&nbsp;°C<br>";
+                                        txt1 += "Current&nbsp;" + u[i].childNodes[0].nodeValue + "&nbsp;°C<br>";
                                     }
                                 }
                                 if (x < 1) {
                                     document.getElementsByClassName("f")[x].innerHTML = date;
-                                    document.getElementsByClassName("z")[x].innerHTML = p.childNodes[0].nodeValue + txt0;
+                                    document.getElementsByClassName("z")[x].innerHTML = txt + txt0 + txt1;
                                 } else if (x >= 1) {
                                     document.getElementsByClassName("f")[x].innerHTML = date;
-                                    document.getElementsByClassName("z")[x].innerHTML = p.childNodes[0].nodeValue + txt;
+                                    document.getElementsByClassName("z")[x].innerHTML = txt + txt0;
                                 }
                             }
                         } else {
