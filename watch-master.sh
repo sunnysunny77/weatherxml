fbd7722e18eb3601d3c5c3bfa61ec034af590be4
daniel <contractor@lateralaspect.com.au>
2024-08-03 00:05:09 +0800

    up

--- watch-master.sh
+++ watch-master.sh
@@ -4,19 +4,21 @@ set -e
 
 set -x
 
-npm run php-server &
+npm run watch-js &
 
 S1=$!
 
-npm run https-server &
+npm run php-server &
 
 S2=$!
 
-npm run watch-js &
+npm run https-server &
+
+S3=$!
 
 wait
  
-kill $S1 $S2 
+kill $S1 $S2 $S3
 
 
 
