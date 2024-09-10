d88fc1c5872307382522b608fa8470f0be66be26
daniel <contractor@lateralaspect.com.au>
2024-09-10 09:47:25 +0800

    Update worker.js

--- site/worker.js
+++ site/worker.js
@@ -1,51 +1,77 @@
-self.addEventListener("install", function(event) {
+const version = 1;
+const cacheName = `weather-v${version}`;
+
+const cacheAssets = [
+  "./",
+  "./index.php",
+  "./css/app.min.css",
+  "./js/app.min.js",
+  "./font/NunitoSans.ttf",
+  "./webfonts/fa-regular-400.woff2",
+  "./images/1.webp",
+  "./images/2.webp",
+  "./images/3.webp",
+  "./images/4.webp",
+  "./images/6.webp",
+  "./images/8.webp",
+  "./images/9.webp",
+  "./images/10.webp",
+  "./images/11.webp",
+  "./images/12.webp",
+  "./images/13.webp",
+  "./images/14.webp",
+  "./images/15.webp",
+  "./images/16.webp",
+  "./images/17.webp",
+  "./images/18.webp",
+  "./images/19.webp",
+  "./images/sticky.webp",
+  "./IDW12300.xml",
+  "./IDW60920.xml"
+];
+
+self.addEventListener("install", (event) => {
+  console.log("Service worker is installed");
 
   event.waitUntil(
-    caches.open("v1").then(function(cache) {
-      return cache.addAll([
-        "./",
-        "./index.php",
-        "./css/app.min.css",
-        "./js/app.min.js",
-        "./font/NunitoSans.ttf",
-        "./webfonts/fa-regular-400.woff2",
-        "./images/1.webp",
-        "./images/2.webp",
-        "./images/3.webp",
-        "./images/4.webp",
-        "./images/6.webp",
-        "./images/8.webp",
-        "./images/9.webp",
-        "./images/10.webp",
-        "./images/11.webp",
-        "./images/12.webp",
-        "./images/13.webp",
-        "./images/14.webp",
-        "./images/15.webp",
-        "./images/16.webp",
-        "./images/17.webp",
-        "./images/18.webp",
-        "./images/19.webp",
-        "./images/sticky.webp",
-        "./IDW12300.xml",
-        "./IDW60920.xml"
-      ]);
+    caches
+      .open(cacheName)
+      .then((cache) => {
+        console.log("Caching assets");
+        cache.addAll(cacheAssets);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("Service worker is activated");
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return cacheNames.map((cache) => {
+        if (cache !== cacheName) {
+          console.log("Clearing old caches");
+          caches.delete(cache);
+        }
+      });
     })
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
+  console.log("Fetching via Service worker");
 
-  event.respondWith(caches.open("v1").then((cache) => {
-
-      return fetch(event.request).then((networkResponse) => {
-
-        cache.put(event.request, networkResponse.clone());
-
-        return networkResponse;
-      }).catch(() => {
-
+  event.respondWith(
+    fetch(event.request)
+      .then((networkResponse) => {
+        return caches.open(cacheName).then((cache) => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      })
+      .catch(() => {
         return caches.match(event.request);
-    } );
-  }));
+      })
+  );
 });
