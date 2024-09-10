9a6e30ab8af5847cb776008fc03b608143c4ac8b
daniel <contractor@lateralaspect.com.au>
2024-09-10 19:00:11 +0800

    Update worker.js

--- site/worker.js
+++ site/worker.js
@@ -32,32 +32,32 @@ const cacheAssets = [
 ];
 
 self.addEventListener("install", (event) => {
+
   console.log("Service worker is installed");
 
-  event.waitUntil(
-    caches
-      .open(cacheName)
-      .then((cache) => {
-        console.log("Caching assets");
-        cache.addAll(cacheAssets);
-      })
-      .then(() => self.skipWaiting())
-  );
+  event.waitUntil(caches.open(cacheName).then((cache) => {
+
+    console.log("Caching assets");
+    cache.addAll(cacheAssets);
+  }).then(() => {
+
+    self.skipWaiting();
+  }));
 });
 
 self.addEventListener("fetch", (event) => {
+
   console.log("Fetching via Service worker");
 
-  event.respondWith(
-    fetch(event.request)
-      .then((networkResponse) => {
-        return caches.open(cacheName).then((cache) => {
-          cache.put(event.request, networkResponse.clone());
-          return networkResponse;
-        });
-      })
-      .catch(() => {
-        return caches.match(event.request);
-      })
-  );
+  event.respondWith(fetch(event.request).then((networkResponse) => {
+    
+    return caches.open(cacheName).then((cache) => {
+
+      cache.put(event.request, networkResponse.clone());
+      return networkResponse;
+    });
+  }).catch(() => {
+    
+    return caches.match(event.request);
+  }));
 });
