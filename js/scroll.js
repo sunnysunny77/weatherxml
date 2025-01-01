dda47a4f419ff35d91de47908a2432c99972e91f
daniel <contractor@lateralaspect.com.au>
2025-01-01 15:09:57 +0800

    up

--- js/scroll.js
+++ js/scroll.js
@@ -16,18 +16,26 @@ export const scroll = () => {
 
             const before_start = body.classList.contains("before-start");
             const before_i = body.classList.contains(`before-${i}`);
+            const before_end = body.classList.contains("before-end");
+            const reverse = body.classList.contains("reverse");
 
              if (scrollY > get_position(index) && scrollY < get_position(index) + index.scrollHeight) {   
-            
+                
+                if (before_start) body.classList.remove("before-start");       
+                if (before_end) body.classList.remove("before-end"); 
+                if (scrollY < prev_scroll && !before_i) return body.classList.add(`before-${i}`, "reverse");
+                if (reverse && !before_i) body.classList.remove("reverse");
                 if (!before_i) body.classList.add(`before-${i}`);
-                if (before_start) body.classList.remove("before-start");   
             } else  if (scrollY > get_position(main) + main.scrollHeight) {
 
                 if (before_i) body.classList.remove(`before-${i}`);
-                if (!before_start) body.classList.add("before-start");    
-            }  else  if (scrollY < prev_scroll) {
+                if (!before_end) body.classList.add("before-end"); 
+                if (reverse) body.classList.remove("reverse");
+            }  else  if (scrollY < prev_scroll && scrollY < get_position(main)) {
 
                 if (before_i) body.classList.remove(`before-${i}`);
+                if (before_end) body.classList.remove("before-end"); 
+                if (reverse) body.classList.remove("reverse");
                 if (!before_start && before_i) body.classList.add("before-start");    
             }  else {
 
