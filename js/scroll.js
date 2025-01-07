25f1af887c5bb1b4ae8e5cbb13dd28a2159cdafc
daniel <contractor@lateralaspect.com.au>
2025-01-07 17:34:43 +0800

    up

--- js/scroll.js
+++ js/scroll.js
@@ -26,23 +26,28 @@ export const scroll = () => {
                 if (before_start) body.classList.remove("before-start");       
                 if (before_end) body.classList.remove("before-end"); 
                 if (scrollY < prev_scroll) {
-                    if (!before_i) body.classList.add(`before-${i}`, "reverse");
-                    if (before_i_plus) body.classList.remove(`before-${i + 1}`);
+                    if (before_i_plus && !before_i) { 
+                        body.classList.replace(`before-${i + 1}`, `before-${i}`);
+                    } else if (scrollY < prev_scroll && !before_i_plus && !before_i) {
+                        body.classList.add(`before-${i}`);
+                    }
+                    if (!reverse && !before_i) body.classList.add("reverse");
+                } else if (scrollY > prev_scroll) {
+                    if (reverse && !before_i) body.classList.remove("reverse");
+                    if (before_i_minus && !before_i) {
+                        body.classList.replace(`before-${i - 1}`, `before-${i}`);
+                    } else if (!before_i_minus && !before_i) {
+                        body.classList.add(`before-${i}`);
+                    }
                 }
-                if (reverse && !before_i) body.classList.remove("reverse");
-                if (!before_i) body.classList.add(`before-${i}`) 
-                if (before_i_minus) body.classList.remove(`before-${i - 1}`);
             } else  if (scrollY > get_position(main) + main.scrollHeight) {
 
-                if (before_i) body.classList.remove(`before-${i}`);
-                if (!before_end) body.classList.add("before-end"); 
+                if (before_i && !before_end) body.classList.replace(`before-${i}`, "before-end");
                 if (reverse) body.classList.remove("reverse");
             }  else  if (scrollY < prev_scroll && scrollY < get_position(main)) {
 
-                if (before_i) body.classList.remove(`before-${i}`);
-                if (before_end) body.classList.remove("before-end"); 
+                if (before_i && !before_start) body.classList.replace(`before-${i}`, "before-start");
                 if (reverse) body.classList.remove("reverse");
-                if (!before_start && before_i) body.classList.add("before-start");    
             } 
         }
         prev_scroll = scrollY;
