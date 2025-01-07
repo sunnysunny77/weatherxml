a7c7378972739509e533f76b26bb0e23e70d8ae1
daniel <contractor@lateralaspect.com.au>
2025-01-07 18:37:23 +0800

    Update scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -23,39 +23,54 @@ export const scroll = () => {
 
              if (scrollY > get_position(index) && scrollY < get_position(index) + index.scrollHeight) {                   
 
-                if (before_start) body.classList.remove("before-start");       
+                if (before_start) body.classList.remove("before-start");    
+
                 if (before_end) body.classList.remove("before-end"); 
+
                 if (scrollY < prev_scroll) {
-                    
+
                     if (before_i_plus && !before_i) { 
 
                         body.classList.replace(`before-${i + 1}`, `before-${i}`);
+
                     } else if (scrollY < prev_scroll && !before_i_plus && !before_i) {
 
                         body.classList.add(`before-${i}`);
+
                     }
+
                     if (!reverse && !before_i) body.classList.add("reverse");
+
                 } else if (scrollY > prev_scroll) {
 
                     if (reverse && !before_i) body.classList.remove("reverse");
+
                     if (before_i_minus && !before_i) {
 
                         body.classList.replace(`before-${i - 1}`, `before-${i}`);
+
                     } else if (!before_i_minus && !before_i) {
 
                         body.classList.add(`before-${i}`);
+
                     }
                 }
+                
             } else  if (scrollY > get_position(main) + main.scrollHeight) {
 
                 if (before_i && !before_end) body.classList.replace(`before-${i}`, "before-end");
+
                 if (reverse) body.classList.remove("reverse");
+
             }  else  if (scrollY < prev_scroll && scrollY < get_position(main)) {
 
                 if (before_i && !before_start) body.classList.replace(`before-${i}`, "before-start");
+
                 if (reverse) body.classList.remove("reverse");
+
             } 
         }
+        
         prev_scroll = scrollY;
     });
 };
