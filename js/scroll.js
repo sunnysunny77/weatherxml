f38d3d90269abc24c1f3d570037e8a6f33b55ac6
daniel <contractor@lateralaspect.com.au>
2025-01-07 12:44:14 +0800

    up

--- js/scroll.js
+++ js/scroll.js
@@ -10,22 +10,28 @@ export const scroll = () => {
         const u = document.querySelectorAll(".u");
         const body = document.querySelector("body");
         const main = document.querySelector("main");
+        const before_end = body.classList.contains("before-end");
+        const reverse = body.classList.contains("reverse");
+        const before_start = body.classList.contains("before-start");
         const scrollY = window.scrollY;
 
         for (const [i, index] of u.entries()) {
 
-            const before_start = body.classList.contains("before-start");
             const before_i = body.classList.contains(`before-${i}`);
-            const before_end = body.classList.contains("before-end");
-            const reverse = body.classList.contains("reverse");
+            const before_i_plus = body.classList.contains(`before-${i + 1}`);
+            const before_i_minus = body.classList.contains(`before-${i - 1}`);
 
-             if (scrollY > get_position(index) && scrollY < get_position(index) + index.scrollHeight) {   
-                
+             if (scrollY > get_position(index) && scrollY < get_position(index) + index.scrollHeight) {                   
+
                 if (before_start) body.classList.remove("before-start");       
                 if (before_end) body.classList.remove("before-end"); 
-                if (scrollY < prev_scroll && !before_i) return body.classList.add(`before-${i}`, "reverse");
+                if (scrollY < prev_scroll) {
+                    if (!before_i) body.classList.add(`before-${i}`, "reverse");
+                    if (before_i_plus) body.classList.remove(`before-${i + 1}`);
+                }
                 if (reverse && !before_i) body.classList.remove("reverse");
-                if (!before_i) body.classList.add(`before-${i}`);
+                if (!before_i) body.classList.add(`before-${i}`) 
+                if (before_i_minus) body.classList.remove(`before-${i - 1}`);
             } else  if (scrollY > get_position(main) + main.scrollHeight) {
 
                 if (before_i) body.classList.remove(`before-${i}`);
@@ -37,10 +43,7 @@ export const scroll = () => {
                 if (before_end) body.classList.remove("before-end"); 
                 if (reverse) body.classList.remove("reverse");
                 if (!before_start && before_i) body.classList.add("before-start");    
-            }  else {
-
-                if (before_i) body.classList.remove(`before-${i}`);
-            }
+            } 
         }
         prev_scroll = scrollY;
     });
