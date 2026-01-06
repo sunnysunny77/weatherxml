610eb9369f3281badce8e830e0634a62eff96d23
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2026-01-06 19:01:47 +0800

    up

--- js/scroll.js
+++ js/scroll.js
@@ -8,7 +8,6 @@ const backChange = () => {
         const body = document.querySelector("body");
         const main = document.querySelector("main");
         const before_end = body.classList.contains("before-end");
-        const reverse = body.classList.contains("reverse");
         const before_start = body.classList.contains("before-start");
         const scrollY = window.scrollY;
 
@@ -18,22 +17,13 @@ const backChange = () => {
 
             if (scrollY > get_position(index) && scrollY < get_position(index) + index.scrollHeight) {
 
-                body.classList.add(`before-${i}`);
+                if (!before_i) body.classList.add(`before-${i}`);
 
                 for (const x of u.keys()) {
 
                     if(x !== i) {
                         body.classList.remove(`before-${x}`);
                     }
-                }
-
-                if (scrollY < prev_scroll) {
-
-                    if (!reverse && !before_i) body.classList.add("reverse");
-
-                } else if (scrollY > prev_scroll) {
-
-                    if (reverse) body.classList.remove("reverse");
                 }
 
                 if (before_start) body.classList.remove("before-start");
@@ -47,8 +37,6 @@ const backChange = () => {
 
                     body.classList.remove(`before-${x}`);
                 }
-
-                if (reverse) body.classList.remove("reverse");
             }  else  if (scrollY < prev_scroll && scrollY < get_position(main)) {
 
                 if (!before_start && before_i) body.classList.add("before-start");
@@ -57,8 +45,6 @@ const backChange = () => {
 
                     body.classList.remove(`before-${x}`);
                 }
-
-                if (reverse) body.classList.remove("reverse");
             } 
         }
         
